150728424125ecddeb59fc997d6e593df60a14c4
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2023-03-08 03:41:38 +0530

    1

---
+++ src/test/java/com/productmanagement/productmanagmentbackend/ProductManagmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productmanagement.productmanagmentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
